00ac5e319cbb746863ee1ee11d8c3ed200f35af8
Navodya De Silva <125857483+NavodyaD@users.noreply.github.com>
2024-11-30 01:37:44 +0530

    Get Started / Traveler Profile Pages Interface
    Get Started / Traveler Profile Pages Interface

--- app/src/main/java/com/example/tourio/MainActivity.kt
+++ app/src/main/java/com/example/tourio/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.tourio
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_getstarted)
+
+        val getStartedButton: Button = findViewById(R.id.getstarted_button)
+
+            getStartedButton.setOnClickListener {
+                val intent = Intent(this, HomePageActivity::class.java)
+                startActivity(intent)
+            }
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.feedbackpage)) { v, insets ->
+            //val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            //insets
+        //}
     }
 }
