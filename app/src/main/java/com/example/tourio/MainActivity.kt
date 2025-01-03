8ccfc0808feeb1876171b104aa91ca00ef00dc80
nethmiTK <nethmitk33@gmail.com>
2025-01-03 22:52:22 +0530

    main activity

--- app/src/main/java/com/example/tourio/MainActivity.kt
+++ app/src/main/java/com/example/tourio/MainActivity.kt
@@ -1,68 +1,36 @@
 package com.example.tourio
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: HotelsAdapter
-    private val hotelsList = mutableListOf<Hotel>()
-
-    @SuppressLint("MissingInflatedId")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hotelspage)
+        setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.myrecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        replacementFragment(HomeFragment())
 
-        adapter = HotelsAdapter(hotelsList) { userId ->
-            // navigate user to hotel details page with userId
-            val intent = Intent(this, HotelProfileUserViewActivity::class.java)
-            intent.putExtra("userId", userId)
-            startActivity(intent)
-        }
-        recyclerView.adapter = adapter
+        findViewById<BottomNavigationView>(R.id.bottom_menue).setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homepage -> replacementFragment(HomeFragment())
+                R.id.hotel -> replacementFragment(HotelFragment())
+                R.id.request -> replacementFragment(RequestFragment())
+                R.id.profile -> replacementFragment(ProfileFragment())
 
-        fetchHotelsFromFirestore()
 
-        //super.onCreate(savedInstanceState)
 
-        //val getStartedButton: Button = findViewById(R.id.getstarted_button)
-
-        //getStartedButton.setOnClickListener {
-            //val intent = Intent(this, SignUpActivity::class.java)
-            //startActivity(intent)
-        //}
-        // -------------------
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.feedbackpage)) { v, insets ->
-        //val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //insets
-        //}
-    }
-
-    private fun fetchHotelsFromFirestore() {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("Hotels").get()
-            .addOnSuccessListener { result ->
-                hotelsList.clear()
-                for (document in result) {
-                    val hotel = document.toObject(Hotel::class.java)
-                    hotelsList.add(hotel)
-                }
-                adapter.notifyDataSetChanged()
+                else -> false
             }
+            true
+        }
     }
 
-    fun navigateToLogin(view: View) {}
+    private fun replacementFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 }
