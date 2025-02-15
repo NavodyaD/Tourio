eb2e6fd5fa0cf8f5397882d0f3f7db2b85a7ee20
Navodya De Silva <kdnavodya7@gmail.com>
2025-02-15 14:26:49 +0530

    Unnecessary Hardcode removal
    Unnecessary hotel page hardcode tours removal

--- app/src/main/java/com/example/tourio/MainActivity.kt
+++ app/src/main/java/com/example/tourio/MainActivity.kt
@@ -15,31 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         val getStartedButton: Button = findViewById(R.id.getstarted_button)
         getStartedButton.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+           val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-
-
-        //replacementFragment(HomeFragment())
-
-        //findViewById<BottomNavigationView>(R.id.bottom_menue).setOnItemSelectedListener { item ->
-            //when (item.itemId) {
-                //R.id.homepage -> replacementFragment(HomeFragment())
-                //R.id.hotel -> replacementFragment(HotelFragment())
-                //R.id.request -> replacementFragment(RequestFragment())
-                //R.id.profile -> replacementFragment(ProfileFragment())
-
-
-
-               // else -> false
-            //}
-            //true
-        //}
     }
 
-    //private fun replacementFragment(fragment: Fragment) {
-        //val fragmentTransaction = supportFragmentManager.beginTransaction()
-        //fragmentTransaction.replace(R.id.frame_layout, fragment)
-        //fragmentTransaction.commit()
-    //}
 }
